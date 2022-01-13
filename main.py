3ca7abfd3295b6afd0cc622d67443161e0dd9254
Michael G. Lerner <mglerner@pm.me>
2022-01-13 12:53:13 -0500

    refactored, I think it actually works. Time to let Xehr give it a run.

--- main.py
+++ main.py
@@ -30,26 +30,53 @@ from SecretInfo import TOKEN, GUILD_IDS
 
 bot = discord.Bot()
 
+
+@bot.slash_command(guild_ids=GUILD_IDS,description="Help message for PoGoQuizBot")
+async def pqhelp(ctx):
+    help_message = f"""Welcome to the Pokemon Go Quiz Bot!
+/qa rock
+gives you a quiz about rock as an attacker against all other types.
+/qa rock 2
+gives you a quiz about rock as an attacker against two other types.
+/qa random
+gives you a quiz about a random type as an attacker against all other types.
+
+/qd command do the same as qa, except that you specify the defender rather than the attacker.
+
+This quiz uses emojis to represent type effectiveness. Here's what the emojis mean:
+{effectiveness_to_emoji['double resisted']}: double resisted
+{effectiveness_to_emoji['not very effective']}: not very effective
+{effectiveness_to_emoji['neutral']}: neutral
+{effectiveness_to_emoji['super effective']}: super effective
+"""
+    await ctx.respond(help_message)
+
+
 CATEGORY_NAME = 'POGOQUIZ'
-CHANNEL_ACTIVITY = {}
 CHANNEL_TIMEOUT = 600 # in seconds
 QUIZ_TIMEOUT = 15.0
 
-def _add_channel_activity(c,created_at,last,channel):
-    CHANNEL_ACTIVITY[c] = {'created':created_at,'last':created_at,'channel':channel}
+CHANNEL_ACTIVITY = {}
+def record_channel_activity(channel):
+    """Record the fact that we have seen activity in a channel.
 
-def record_channel_activity(ctx):
-    if not check_channel(ctx):
-        return
-    c = str(ctx.channel)
-    if c not in CHANNEL_ACTIVITY:
-        _add_channel_activity(c=c,created_at=ctx.channel.created_at,last=ctx.channel.created_at,channel=ctx.channel)
-        CHANNEL_ACTIVITY[c] = {'created':ctx.channel.created_at,'last':ctx.channel.created_at,'channel':ctx.channel}
-    CHANNEL_ACTIVITY[c]['last'] = datetime.now()
+    This is so that another function can delete inactive channels. That
+    function should only delete channels in CATEGORY_NAME. But, out of
+    pure paranoia, we'll only record channels in that category anyway.
+    """
+    if str(channel.category) != CATEGORY_NAME:
+        print(f'Oh trouble, {channel.category} != {CATEGORY_NAME}') #MGL: TODO: FIXME
+    #category = discord.utils.get(ctx.guild.categories, name=CATEGORY_NAME)
+
+    if channel not in CHANNEL_ACTIVITY:
+        CHANNEL_ACTIVITY[channel] = {'created':datetime.now(),'last':datetime.now()}
+    CHANNEL_ACTIVITY[channel]['last'] = datetime.now()
 
 def get_private_channel_name(ctx):
     """
     No special characters, has to be all lowercase. Discord.py will make it all lowercase regardless.
+
+    TODO: does this work for multiple servesr (guilds)? Make my own and check.
     """
     stripped_name = str(ctx.author)
     stripped_name = ''.join(i for i in stripped_name if i.isalnum())
@@ -76,144 +103,127 @@ async def create_channel(ctx):
     user = ctx.author.id
     category = discord.utils.get(ctx.guild.categories, name=CATEGORY_NAME)
     if category is None:
-        category = await ctx.guild.create_category(category_name,reason=f'Creating PoGo quiz category')
+        category = await ctx.guild.create_category(CATEGORY_NAME,reason=f'Creating PoGo quiz category')
     channel = await ctx.guild.create_text_channel(channel_name,
                                                       overwrites=overwrites, reason=f"Creating PoGo quiz channel for user {ctx.author}",
                                                       category=category)
 
-# Obviously qa and qd should be refactored. The main reason that they
-# aren't is because of the `break` for the timeout in the middle of
-# the while loop.
-@bot.slash_command(guild_ids=GUILD_IDS,description="Attacker: normal, rock, random ...")
-async def qa(ctx, attacker: str, num_questions:int=18,):
+async def check_channel_and_redirect_user(ctx):
+    # Maybe also ping in the channel.
     if not check_channel(ctx):
         if not discord.utils.get(ctx.guild.channels,name=get_private_channel_name(ctx)):
             await create_channel(ctx)
-        txt = f'{ctx.author}, ask for your quiz in {get_private_channel(ctx).mention}!\n(The bot will only respond there, not in {ctx.channel.mention}).'
+        txt = f'{ctx.author}, your quiz in {get_private_channel(ctx).mention}!\n(asking for future quizzes there will help reduce clutter in {ctx.channel.mention}).'
         await ctx.respond(txt)
         return
-    if num_questions > len(effectiveness) or num_questions < 1:
-        await ctx.respond(f'You asked for {num_questions} questions, but there are only {len(effectiveness)} types, so I want a number between 1 and {len(effectiveness)}. I will ask you {len(effectiveness)} questions')
-        num_questions = len(effectiveness)
-    random_attacker = False
+
+async def get_attackers_and_defenders(attacker, defender, num_questions, ctx):
+    random_attacker, random_defender = False, False
     if attacker == 'random':
         random_attacker = True
-    defenders = list(effectiveness.keys())
-    random.shuffle(defenders)
-    defenders = defenders[:num_questions]
-    question_count = 0
-    while question_count < num_questions:
-        defender = defenders.pop()
-        if random_attacker:
-            attacker = random.choice(list(effectiveness.keys()))
-        record_channel_activity(ctx)
-        if attacker not in effectiveness or defender not in effectiveness:
-            await ctx.respond(f'Sorry, I could not find one of the two types you wanted: {attacker} or {defender}')
-            break
+    if defender == 'random':
+        random_defender = True
+    if attacker != 'random' and attacker not in efectiveness:
+        await ctx.respond(f"Attacker type {attacker} unknown. \nI'm choosing rock because nothing beats that.\nPlease either use 'random' or one of {list(effectiveness.keys())}")
 
+async def ask_type_questions(attackers, defenders, channel, question_mode, guesser):
+    """Ask the questions.
+
+    question_mode can be attacker or defender
+
+    guesser is the person who is guessing. This listens to every single
+    reaction anywhere, so we need to know which ones actually count.
+
+    No exceptions defined here. Can raise asyncio.TimeoutError
+    """
+    msg = await channel.send("Here's your quiz")
+    if question_mode not in ('attacker','defender'):
+        question_mode = 'attacker'
+    for pogotype in attackers + defenders:
+        if pogotype not in effectiveness:
+            await msg.reply(f'Sorry, I could not find one of the types you wanted: {pogotype}')
+            return
+    for (attacker, defender) in zip(attackers,defenders):
+        record_channel_activity(channel)
         right_answer = effectiveness[attacker][defender]
         right_answer_emoji = effectiveness_to_emoji[effectiveness_to_words[right_answer]]
-
-        # OK, this is baffling to me. The first time through, it looks like you want orgmsg.
-        # But then orgmsg never changes. So, on subsequent iterations, we want resp.
-        # AND I DON'T KNOW HOW TO GET THAT! So, a really fragile check to see if it has the add_reaction method seems to work.
-        resp = await ctx.respond(f'damage is ??? when {attacker} is attacking against {defender}')
-        orgmsg = await ctx.interaction.original_message()
-        if hasattr(resp,'add_reaction'):
-            mymsg = resp
+        if question_mode == 'attacker':
+            mymsg = await msg.reply(f'damage is ??? when {attacker} is attacking against {defender}')
         else:
-            mymsg = orgmsg
+            mymsg = await msg.reply(f'damage is ??? when {defender} is defending against {attacker}')
         for i in ('double resisted','not very effective','neutral','super effective'):
             await mymsg.add_reaction(emoji=effectiveness_to_emoji[i])
+
         def check(reaction,user):
             """If you give a check function, it ignores all
             reactions that fail the check. So they can react a bunch
             of wrong things and you'll wait for the right one.
             """
-            result = (user == ctx.author and str(reaction.emoji) == right_answer_emoji)
+            result = (user == guesser) and (str(reaction.emoji) == right_answer_emoji)
             return result
+
         try:
             reaction, user = await bot.wait_for('reaction_add', timeout=QUIZ_TIMEOUT, check=check)
         except asyncio.TimeoutError:
-            await ctx.channel.send('You timed out')
+            await mymsg.channel.send('You timed out')
             break
         else:
             await mymsg.add_reaction(emoji='👍')
-            question_count = question_count + 1
-    await ctx.channel.send('The quiz is over!')
-
-# Obviously qa and qd should be refactored. The main reason that they
-# aren't is because of the `break` for the timeout in the middle of
-# the while loop.
-@bot.slash_command(guild_ids=GUILD_IDS,description="Defender: normal, rock, random ...")
-async def qd(ctx, defender: str, num_questions:int=18,):
-    if not check_channel(ctx):
-        if not discord.utils.get(ctx.guild.channels,name=get_private_channel_name(ctx)):
-            await create_channel(ctx)
-        txt = f'{ctx.author}, ask for your quiz in {get_private_channel(ctx).mention}!\n(The bot will only respond there, not in {ctx.channel.mention}).'
-        await ctx.respond(txt)
-        return
+            
+@bot.slash_command(guild_ids=GUILD_IDS,description="Attacker: normal, rock, random ...")
+async def qa(ctx, attacker: str, num_questions:int=18,):
+    await check_channel_and_redirect_user(ctx)
+    channel = get_private_channel(ctx)
+    
+    # Set up lists of attackers and defenders
     if num_questions > len(effectiveness) or num_questions < 1:
         await ctx.respond(f'You asked for {num_questions} questions, but there are only {len(effectiveness)} types, so I want a number between 1 and {len(effectiveness)}. I will ask you {len(effectiveness)} questions')
         num_questions = len(effectiveness)
-    random_defender = False
+
+    if attacker == 'random':
+        attackers = [random.choice(list(effectiveness.keys())) for i in num_questions]
+    else:
+        attackers = [attacker]*num_questions
+    defenders = list(effectiveness.keys())
+    random.shuffle(defenders)
+    defenders = defenders[:num_questions]
+
+    # Ask N questions.
+    try:
+        await ask_type_questions(attackers, defenders, channel, question_mode='attacker', guesser=ctx.user)
+    except asyncio.TimeoutError:
+        await channel.send('You timed out') # Where to send this? Could use ctx to send to original channel.
+    await channel.send('The quiz is over!')
+
+@bot.slash_command(guild_ids=GUILD_IDS,description="Defender: normal, rock, random ...")
+async def qd(ctx, defender: str, num_questions:int=18,):
+    await check_channel_and_redirect_user(ctx)
+    channel = get_private_channel(ctx)
+    
+    # Set up lists of attackers and defenders
+    if num_questions > len(effectiveness) or num_questions < 1:
+        await ctx.respond(f'You asked for {num_questions} questions, but there are only {len(effectiveness)} types, so I want a number between 1 and {len(effectiveness)}. I will ask you {len(effectiveness)} questions')
+        num_questions = len(effectiveness)
+
     if defender == 'random':
-        random_defender = True
+        defenders = [random.choice(list(effectiveness.keys())) for i in num_questions]
+    else:
+        defenders = [defender]*num_questions
     attackers = list(effectiveness.keys())
     random.shuffle(attackers)
     attackers = attackers[:num_questions]
-    question_count = 0
-    while question_count < num_questions:
-        attacker = attackers.pop()
-        if random_defender:
-            defender = random.choice(list(effectiveness.keys()))
-        record_channel_activity(ctx)
-        if attacker not in effectiveness or defender not in effectiveness:
-            await ctx.respond(f'Sorry, I could not find one of the two types you wanted: {attacker} or {defender}')
-            break
 
-        right_answer = effectiveness[attacker][defender]
-        right_answer_emoji = effectiveness_to_emoji[effectiveness_to_words[right_answer]]
-
-        # OK, this is baffling to me. The first time through, it looks like you want orgmsg.
-        # But then orgmsg never changes. So, on subsequent iterations, we want resp.
-        # AND I DON'T KNOW HOW TO GET THAT! So, a really fragile check to see if it has the add_reaction method seems to work.
-        resp = await ctx.respond(f'damage is ??? when {attacker} is attacking against {defender}')
-        orgmsg = await ctx.interaction.original_message()
-        if hasattr(resp,'add_reaction'):
-            mymsg = resp
-        else:
-            mymsg = orgmsg
-        for i in ('double resisted','not very effective','neutral','super effective'):
-            await mymsg.add_reaction(emoji=effectiveness_to_emoji[i])
-        def check(reaction,user):
-            """If you give a check function, it ignores all
-            reactions that fail the check. So they can react a bunch
-            of wrong things and you'll wait for the right one.
-            """
-            result = (user == ctx.author and str(reaction.emoji) == right_answer_emoji)
-            return result
-        try:
-            reaction, user = await bot.wait_for('reaction_add', timeout=10.0, check=check)
-        except asyncio.TimeoutError:
-            await ctx.channel.send('You timed out')
-            break
-        else:
-            await mymsg.add_reaction(emoji='👍')
-            question_count = question_count + 1
-    await ctx.channel.send('The quiz is over!')
+    # Ask N questions.
+    try:
+        await ask_type_questions(attackers, defenders, channel, question_mode='defender', guesser=ctx.user)
+    except asyncio.TimeoutError:
+        await channel.send('You timed out') # Where to send this? Could use ctx to send to original channel.
+    await channel.send('The quiz is over!')
 
 @bot.event
 async def on_ready():
     print("We have logged in as {0.user}".format(bot))
 
-#async def my_background_task():
-#    await bot.wait_until_ready()
-#    print('frog')
-#    while not bot.is_closed:
-#        print('cow')
-#        await asyncio.sleep(1) # task runs every 1 seconds
-#bot.loop.create_task(my_background_task())
 from discord.ext import tasks
 @tasks.loop(seconds=5.0)
 async def cleanup_channels():
@@ -222,52 +232,35 @@ async def cleanup_channels():
     Channels get deleted if they're inactive for longer than
     CHANNEL_TIMEOUT seconds. We could also delete them after a fixed
     period of time regardless.
+
+    We carefully check to make sure the category matches CATEGORY_NAME
+    before actually deleting.
     """
     # Lots of example code uses a while not bot.is_closed loop. I
     # don't think we need that.
+    our_categories = []
 
     for guild in bot.guilds:
         category = discord.utils.get(guild.categories, name=CATEGORY_NAME)
+        our_categories.append(category)
         if category is not None:
             channels = category.channels
             for c in channels:
-                if str(c) not in CHANNEL_ACTIVITY:
-                    _add_channel_activity(c=str(c),created_at=datetime.now(),last=datetime.now,channel=c)
+                if c not in CHANNEL_ACTIVITY:
                     print(f'Oh, I found a channel {c} that was not properly recorded. Fixing.')
+                    record_channel_activity(c)
+                    
     
     channels = list(CHANNEL_ACTIVITY.keys())
     for c in channels:
+        if c.category not in our_categories:
+            print(f'Skipping deletion of {c} because {c.category} is unrecognized')
         time_in_existence = CHANNEL_ACTIVITY[c]['last'] - CHANNEL_ACTIVITY[c]['last']
         time_since_active = datetime.now() - CHANNEL_ACTIVITY[c]['last']
         if time_since_active.seconds > CHANNEL_TIMEOUT:
             print(f'Time to delete {c}')
-            await CHANNEL_ACTIVITY[c]['channel'].delete()
             del CHANNEL_ACTIVITY[c]
+            await c.delete()
 cleanup_channels.start()
 
 bot.run(TOKEN)
-"""Welcome to the Pokemon Go type quiz bot.
-!q1 quizzes one explicit type matchup.
-!q1 rock ground
-tells you how effective rock is when attacking ground.
-!qa quizzes you about one type as an attacker
-!qa rock
-gives you a quiz about rock as an attacker against a random type.
-!qa rock 3
-gives you three quizzes about rock as an attacker against a random type.
-
-!qa steel
-gives you a quiz about steel as an defender against a random type.
-!qa steel 3
-gives you three quizzes about steel as an defender against a random type.
-
-You can also run quizzes against random types. For example
-!qa random 3
-gives you three quizzes about random types as attackers against random types
-
-This quiz uses emojis to represent type effectiveness. Here's what the emojis mean:
-{effectiveness_to_emoji['double resisted']}: double resisted
-{effectiveness_to_emoji['not very effective']}: not very effective
-{effectiveness_to_emoji['neutral']}: neutral
-{effectiveness_to_emoji['super effective']}: super effective
-"""
