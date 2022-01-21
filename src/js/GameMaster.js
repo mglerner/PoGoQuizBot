6d2a3086b971878d79f8d59554cb8a3ec995bd7b
Michael G. Lerner <mglerner@pm.me>
2022-01-21 08:58:21 -0800

    Squashed 'external/pvpoke/' changes from a6f1ec51..b754330e
    b754330e Fixed issues with Little League option disappearing
    f18575c7 Update notes, removed Multi Battle link from custom rankings
    86002dff Custom Ranking dropdown update, import fixes
    ccd53db4 Merge branch 'ranking-detail-tabs'
    9ff6f514 Updated URL parameters, fixed changing leagues
    1fc4b5a6 Marked Mega Steelix as released
    64e0b0f5 Multi-Battle Meta toggle
    440249c6 Updated Great League and Ultra League training teams with Walrein
    2977c277 Removed Walrein from Fusion Cup
    de350ea4 Added Heliolisk family, updated rankings
    a3726bad Updated Parabolic Charge stats
    e5f9652e Updated Helioptile moveset
    63f7740e Sinnoh top performers update
    20b6bd92 Updated Icicle Spear abbreviation
    44aace31 Added Walrein to meta groups
    9c83a5c6 Updated Gymbreakers Comet Cup for Walrein
    c0eca69b Updated Poliwrath moveset
    f5798e00 Added Icicle Spear, updated rankings
    9862f8e7 Removed old meta groups, added Love Cup list from last year
    1ced0c34 Added Shock Drive Genesect
    35277aca Ranking details setting
    b9bdeadd Ranking tab icons
    0c9c02d3 Tab HTML, styling, and functionality
    
    git-subtree-dir: external/pvpoke
    git-subtree-split: b754330e9da8910668cd0ebb0ac10e9ae89bdfda

--- src/js/GameMaster.js
+++ src/js/GameMaster.js
@@ -36,6 +36,10 @@ var GameMaster = (function () {
 				object.data.pokemon.sort((a,b) => (a.speciesName > b.speciesName) ? 1 : ((b.speciesName > a.speciesName) ? -1 : 0));
 
 				InterfaceMaster.getInstance().init(object);
+
+				if(customRankingInterface){
+					customRankingInterface.init(object);
+				}
 			} else if(settings.gamemaster == "gamemaster-mega"){
 				// Load additional mega pokemon
 				$.getJSON( webRoot+"data/megas.json?v="+siteVersion, function( data ){
@@ -717,7 +721,7 @@ var GameMaster = (function () {
 				minStats = 0;
 			}
 
-			var bannedList = ["mewtwo","mewtwo_armored","giratina_altered","groudon","kyogre","palkia","dialga","heatran","giratina_origin","darkrai","cobalion","terrakion","virizion","thundurus_incarnate","regigigas","tornadus_incarnate","tornadus_therian","tornadus_therian_xl","landorus_incarnate", "landorus_therian", "reshiram", "zekrom", "kyurem", "genesect_burn", "xerneas", "thundurus_therian", "yveltal", "meloetta_aria", "zacian", "zamazenta", "zacian_hero", "zamazenta_hero", "genesect_douse", "zarude", "hoopa_unbound"];
+			var bannedList = ["mewtwo","mewtwo_armored","giratina_altered","groudon","kyogre","palkia","dialga","heatran","giratina_origin","darkrai","cobalion","terrakion","virizion","thundurus_incarnate","regigigas","tornadus_incarnate","tornadus_therian","tornadus_therian_xl","landorus_incarnate", "landorus_therian", "reshiram", "zekrom", "kyurem", "genesect_burn", "xerneas", "thundurus_therian", "yveltal", "meloetta_aria", "zacian", "zamazenta", "zacian_hero", "zamazenta_hero", "genesect_douse", "zarude", "hoopa_unbound", "genesect_shock"];
 
 			// Aggregate filters
 
